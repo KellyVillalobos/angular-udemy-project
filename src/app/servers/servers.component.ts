2941432c09bb27c2259d2aeb4c833596d78ba594
Kelly Villalobos <kvdesign1978@gmail.com>
2018-08-07 08:04:16 -0500

    ngIf statements

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,17 +2,35 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-    <app-server></app-server>
-  `,
+  // template: `
+  // <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'Test Server';
+  serverCreated = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
